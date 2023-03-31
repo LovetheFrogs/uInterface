8a11bc84c4ca2fc43183e60f8c4670f224b390d2
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-03-31 19:09:58 +0200

    Fix: Method call

--- gui/home.py
+++ gui/home.py
@@ -6,10 +6,10 @@ class Home:
         master.title("UVa Judge | Home")
 
         master.home_label = ctk.CTkLabel(master, text="Welcome to the UVa Judge app")
-        master.home_label.config(font=("Arial", 20, "bold"))
+        master.home_label.configure(font=("Arial", 20, "bold"))
         master.home_label.grid(row=0, column=1, padx=20, pady=10, sticky="n")
         master.home_text = ctk.CTkLabel(master, text="To use the app, please refer to the README file of the repo. As "
                                                      "starters, you should try login to your UVa account and coding a "
                                                      "problem!", wraplength=800)
-        master.home_text.config(font=("Arial", 14))
+        master.home_text.configure(font=("Arial", 14))
         master.home_text.grid(row=1, column=1, padx=20, pady=20, sticky="n")
