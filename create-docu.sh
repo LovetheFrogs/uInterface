74a5675e3e981c53b3e51532c89ebc589f96fb8e
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-04-03 13:48:12 +0200

    Fix: Wrong index.html generation

--- create-docu.sh
+++ create-docu.sh
@@ -1,5 +1,5 @@
 cargo doc --no-deps
 rm -rf ./docs
 touch target/doc/index.html
-echo "<meta http-equiv\"refresh\" content=\"0; url=build_wheel\">" > target/doc/index.html
+echo "<meta http-equiv\"refresh\" content=\"0; url=u_interface\">" > target/doc/index.html
 cp -r target/doc ./docs
