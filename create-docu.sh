78fe792615c6dd053f87a2da2e4fd4f7d4176117
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-04-03 14:10:25 +0200

    Fix: Script for creating docu

--- create-docu.sh
+++ create-docu.sh
@@ -1,5 +1,10 @@
-cargo doc --no-deps
+cargo doc --document-private-items --no-deps
 rm -rf ./docs
 touch target/doc/index.html
-echo "<meta http-equiv\"refresh\" content=\"0; url=u_interface\">" > target/doc/index.html
+echo "<script>
+        var timer = setTimeout(function() {
+            window.location='u_interface/index.html'
+        }, 0);
+ </script>
+" > target/doc/index.html
 cp -r target/doc ./docs
