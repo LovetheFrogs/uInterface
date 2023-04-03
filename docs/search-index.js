ec3890f1853eefb7d359fcc16569e7e2adcfedd1
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-04-03 13:45:37 +0200

    Feature: Added webpage for rust docu

---
+++ docs/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"u_interface":{"doc":"Module containing different get request to the uHunt API","t":[5],"n":["PyInit_u_interface"],"q":["u_interface"],"d":[""],"i":[0],"f":[[[],1]],"p":[[3,"PyObject"]]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
---
+++ docs/u_interface/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["PyInit_u_interface",""]]};
