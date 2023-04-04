647684c5de7f1e5be18ce56bdc3eb1b052e307ea
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-04-04 08:17:56 +0200

    Feature: Added setup.py file

---
+++ gui/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="uInterface",
+    version="1.0",
+    author="LovetheFrogs",
+    description="GUI application to use as alternative to UVa judge/uHunt websites",
+    python_requires="==3.10",
+    install_requires=[
+        'customtkinter==4.5.2',
+        'matplotlib==3.7.1',
+        'uInterface'
+    ]
+)
